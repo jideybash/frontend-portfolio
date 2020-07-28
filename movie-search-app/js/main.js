bbe126a829e26d2f9c66467fec0819627ea587f0
Babajide Bashiru <66791061+jideybash@users.noreply.github.com>
2020-07-27 22:27:42 -0500

    error fixed // footer updated

--- movie-search-app/js/main.js
+++ movie-search-app/js/main.js
@@ -11,22 +11,23 @@ $(document).ready(function(){
        axios.get('https://www.omdbapi.com/?s='+searchText+'&apikey=b55f1ed3')
             .then((response) =>{
                 console.log(response);
-                let movies = response.data.search;
+                let movies = response.data.Search;
                 let output = '';
                 $.each(movies, (index, movie) => {
                     output += ` 
-                        <div class ="col-md-3">
-                            <div class="container text-center">
-                                <img src="${movie.Poster}">
-                                <h5>${movie.Title}</h5>
-                                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                        <div class ="container card border-primary mb-3" style="max-width: 20rem;">
+                            <div class="card-header">
+                            <h5>${movie.Title}</h5></div>
+                            <div class="card-body">
+                                <img src="${movie.Poster} class="thumbnail">
+                                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary btn-lg btn-block" href="#">Movie Details</a>
                             </div>
-                        </div
+                        </div>
                     `;
 
                 });
 
-                $('#movie').html(output);
+                $('#movies').html(output);
             })
             .catch((err) => {
                 console.log(err);
@@ -39,7 +40,7 @@ $(document).ready(function(){
           return false;
         }
         
-        function getMovies(){
+        function getMovie(){
           let movieId = sessionStorage.getItem('movieId');
         
           axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=b55f1ed3')
@@ -53,30 +54,30 @@ $(document).ready(function(){
                     <img src="${movie.Poster}" class="thumbnail">
                   </div>
                   <div class="col-md-8">
-                    <h2>${movie.Title}</h2>
+                  <h2 class="display-3">${movie.Title}</h2>
                     <ul class="list-group">
-                      <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                      <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                      <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                      <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                      <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                      <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                      <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                      <li class="list-group-item"><div class="list">Genre:</div> ${movie.Genre}</li>
+                      <li class="list-group-item"><div class="list">Released:</div> ${movie.Released}</li>
+                      <li class="list-group-item"><div class="list">Rated:</div> ${movie.Rated}</li>
+                      <li class="list-group-item"><div class="list">IMDB Rating:</div> ${movie.imdbRating}</li>
+                      <li class="list-group-item"><div class="list">Director:</div> ${movie.Director}</li>
+                      <li class="list-group-item"><div class="list">Writer:</div> ${movie.Writer}</li>
+                      <li class="list-group-item"><div class="list">Actors:</div> ${movie.Actors}</li>
                     </ul>
                   </div>
                 </div>
                 <div class="row">
-                  <div class="well">
+                  <div class="jumbotron">
                     <h3>Plot</h3>
                     ${movie.Plot}
                     <hr>
                     <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-                    <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                    <a href="index.html" class="btn btn-default">Go Back To Search <i class="fas fa-sign-out-alt"></i></a>
                   </div>
                 </div>
               `;
         
-              $('#movies').html(output);
+              $('#movie').html(output);
             })
             .catch((err) => {
               console.log(err);
